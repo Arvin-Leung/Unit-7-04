cb0b5bee5dfbe88345f26ce4241c5706ba0d8736
ArvinLeung <arvin.leung@ocsbstudent.ca>
2021-02-22 13:55:48 +0000

    if else changes

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ document.getElementById('click').addEventListener('click', checker)
 function checker () {
   const age = document.getElementById('age').value;
   const date = document.getElementById('date').value;
-  if ((date == 'tuesday' ) || (date == 'thursday') || (age > 12 && age < 21))
+  if ((date == 'tuesday' ) || (date == 'thursday') || (age > 12 && age < 21)) {
     document.getElementById('dis').innerHTML = 'you are eligible for student pricing'
-  else {
+  } else {
     document.getElementById('dis').innerHTML = 'you are not eligible for student pricing'
   }
 }
