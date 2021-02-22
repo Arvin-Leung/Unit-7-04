7244e3286c6b48fc60cccb2260d277a672f975b7
ArvinLeung <arvin.leung@ocsbstudent.ca>
2021-02-22 13:59:51 +0000

    parentheses changes

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 document.getElementById('click').addEventListener('click', checker)
 
 function checker () {
-  const age = document.getElementById('age').value;
-  const date = document.getElementById('date').value;
-  if ((date === 'tuesday' ) || (date === 'thursday') || (age > 12 && age < 21)) {
+  const age = document.getElementById('age').value
+  const date = document.getElementById('date').value
+  if ((date === 'tuesday') || (date === 'thursday') || (age > 12 && age < 21)) {
     document.getElementById('dis').innerHTML = 'you are eligible for student pricing'
   } else {
     document.getElementById('dis').innerHTML = 'you are not eligible for student pricing'
